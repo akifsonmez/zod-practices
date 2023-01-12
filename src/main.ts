58c4aff525a22aedb6bbf2cc5e8526ed8fb54d54
akifsonmez <akifsonmez@outlook.com>
2023-01-12 23:11:52 +0300

    Add basic types and basic validation examples

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const genders = ["male", "female"] as const
+
+const PersonSchema = z.object({
+  name: z.string(),
+  age: z.number().default(18),
+  job: z.string().optional(),
+  isAlive: z.boolean(),
+  gender: z.enum(genders)
+});
+
+type Person = z.infer<typeof PersonSchema>
+
+const person1 = { name: "Akif", isAlive: true, gender: "male" };
+const person2 = { name: 77 };
+
+
+console.log(PersonSchema.safeParse(person1))
+console.log(PersonSchema.safeParse(person2))
+
+console.log(PersonSchema.parse(person1))
+console.log(PersonSchema.parse(person2))
